67f9166015c68468da06133d3b19546f7fc7a3a4
hhh <1340545944@qq.com>
2024-01-24 15:31:49 +0800

    add exports field to template package.json

--- index.cjs
+++ index.cjs
@@ -11,9 +11,7 @@ const { Program } = require('3h-cli'),
 const TEMPLATE_PATH = './template',
     TEMPLATE_ENCODING = 'utf8';
 
-const templateDirectories = [
-    'src',
-];
+const templateDirectories = ['src'];
 
 const templateFiles = [
     'src/index.ts',
@@ -39,39 +37,36 @@ const templateFiles = [
 /**
  * @param {TemplateData} data
  */
-const renderTemplates = async data => {
+const renderTemplates = async (data) => {
     await templateDirectories.reduce(
         (prevPromise, dir) => prevPromise.then(() => fsPromises.mkdir(dir)),
-        Promise.resolve()
+        Promise.resolve(),
     );
     await Promise.all(
-        templateFiles.map(async path => {
+        templateFiles.map(async (path) => {
             let content = await fsPromises.readFile(
                 join(__dirname, TEMPLATE_PATH, path),
-                TEMPLATE_ENCODING
+                TEMPLATE_ENCODING,
             );
             Object.entries(data).forEach(([key, value]) => {
-                content = content.replace(
-                    new RegExp(`__${key}__`, 'g'),
-                    value
-                );
+                content = content.replace(new RegExp(`__${key}__`, 'g'), value);
             });
             await fsPromises.writeFile(path, content);
-        })
+        }),
     );
 };
 
 /**
  * @param {string} name
  */
-const abbr = name => (
-    name.match(/\w+/g)
-        .map(word => word[0].toUpperCase())
-        .join()
-);
+const abbr = (name) =>
+    name
+        .match(/\w+/g)
+        .map((word) => word[0].toUpperCase())
+        .join();
 
 const program = new Program(pkgInfo.name, {
-    title: pkgInfo.description
+    title: pkgInfo.description,
 });
 
 program
@@ -79,50 +74,49 @@ program
         name: '--name',
         alias: '-n',
         value: '<pkg>',
-        help: 'The name of the package'
+        help: 'The name of the package',
     })
     .option({
         name: '--author',
         alias: '-a',
         value: '<name>',
-        help: 'The author of the package'
+        help: 'The author of the package',
     })
     .option({
         name: '--desc',
         alias: '-d',
         value: '<description>',
-        help: 'The description of the package'
+        help: 'The description of the package',
     })
     .option({
         name: '--keywords',
         alias: '-k',
         value: '<words...>',
-        help: 'The keywords of the package'
+        help: 'The keywords of the package',
     })
     .option({
         name: '--repo',
         alias: '-r',
         value: '<repository>',
-        help: 'The repository of the package'
+        help: 'The repository of the package',
     })
     .option({
         name: '--umd',
         alias: '-u',
         value: '<namespace>',
-        help: 'The global namespace to use'
+        help: 'The global namespace to use',
     })
     .option({
         name: '--no-install',
-        help: 'Do not install dependencies instantly'
+        help: 'Do not install dependencies instantly',
     })
     .option({
         name: '--help',
         alias: '-h',
-        help: 'Show help info'
+        help: 'Show help info',
     })
     .parse(process.argv)
-    .then(async args => {
-
+    .then(async (args) => {
         const { options } = args;
 
         if (options.has('--help')) {
@@ -148,7 +142,7 @@ program
             desc: args.getOption('--desc').join(' ') || `This is ${name}.`,
             author: options.get('--author')[0],
             umd: args.getOption('--umd')[0] || abbr(name),
-            year: (new Date()).getFullYear() + '',
+            year: new Date().getFullYear() + '',
         };
 
         await fsPromises.mkdir(name);
@@ -163,63 +157,71 @@ program
          * into .npmignore when installing the package,
          * so this file is added manually
          */
-        await fsPromises.writeFile('.gitignore', [
-            'node_modules',
-            'types',
-            'js',
-            'dist',
-        ].join('\n') + '\n');
-        await fsPromises.writeFile('package.json', JSON.stringify(
-            {
-                name,
-                version: '0.1.0',
-                description: data.desc,
-                type: 'module',
-                module: `./dist/${name}.min.mjs`,
-                main: `./dist/${name}.umd.min.cjs`,
-                types: './index.d.ts',
-                author: data.author,
-                license: 'MIT',
-                scripts: {
-                    'prebuild': 'tsc',
-                    'build': 'rollup -c',
-                    'postbuild': [
-                        'terser',
-                        `dist/${name}.mjs`,
-                        '--config-file terser.json',
-                        '--module',
-                        `-o dist/${name}.min.mjs`,
-                        '&&',
-                        'terser',
-                        `dist/${name}.umd.cjs`,
-                        '--config-file terser.json',
-                        `-o dist/${name}.umd.min.cjs`,
-                        '&&',
-                        'node stat.cjs'
-                    ].join(' '),
-                    docs: 'dts2md "**" "!index.d.ts" -i ./types -o ./docs -l -I Home.md',
+        await fsPromises.writeFile(
+            '.gitignore',
+            ['node_modules', 'types', 'js', 'dist'].join('\n') + '\n',
+        );
+        await fsPromises.writeFile(
+            'package.json',
+            JSON.stringify(
+                {
+                    name,
+                    version: '0.1.0',
+                    description: data.desc,
+                    type: 'commonjs',
+                    module: `./dist/${name}.min.mjs`,
+                    main: `./dist/${name}.umd.min.cjs`,
+                    types: './index.d.ts',
+                    exports: {
+                        types: './index.d.ts',
+                        node: {
+                            import: `./dist/${name}.mjs`,
+                            default: `./dist/${name}.umd.cjs`,
+                        },
+                        default: {
+                            import: `./dist/${name}.min.mjs`,
+                            default: `./dist/${name}.umd.min.cjs`,
+                        },
+                    },
+                    author: data.author,
+                    license: 'MIT',
+                    scripts: {
+                        prebuild: 'tsc',
+                        build: 'rollup -c',
+                        postbuild: [
+                            'terser',
+                            `dist/${name}.mjs`,
+                            '--config-file terser.json',
+                            '--module',
+                            `-o dist/${name}.min.mjs`,
+                            '&&',
+                            'terser',
+                            `dist/${name}.umd.cjs`,
+                            '--config-file terser.json',
+                            `-o dist/${name}.umd.min.cjs`,
+                            '&&',
+                            'node stat.cjs',
+                        ].join(' '),
+                        docs: 'dts2md "**" "!index.d.ts" -i ./types -o ./docs -l -I Home.md',
+                    },
+                    repository:
+                        args.getOption('--repo')[0] || `${data.author}/${name}`,
+                    keywords: args.getOption('--keywords'),
+                    files: ['dist', 'types', 'index.d.ts'],
+                    devDependencies: {
+                        '@babel/core': '^7.17.0',
+                        '@babel/preset-env': '^7.16.0',
+                        'dts2md': '^0.4.0',
+                        'rollup': '^3.7.0',
+                        '@rollup/plugin-babel': '^6.0.0',
+                        'terser': '^5.16.0',
+                        'typescript': '^4.9.0',
+                    },
                 },
-                repository: args.getOption('--repo')[0]
-                    || `${data.author}/${name}`,
-                keywords: args.getOption('--keywords'),
-                files: [
-                    'dist',
-                    'types',
-                    'index.d.ts'
-                ],
-                devDependencies: {
-                    '@babel/core': '^7.17.0',
-                    '@babel/preset-env': '^7.16.0',
-                    dts2md: '^0.4.0',
-                    rollup: '^3.7.0',
-                    '@rollup/plugin-babel': '^6.0.0',
-                    terser: '^5.16.0',
-                    typescript: '^4.9.0',
-                }
-            },
-            null, // replacer
-            2 // indent
-        ));
+                null, // replacer
+                2, // indent
+            ),
+        );
 
         if (options.has('--no-install')) {
             console.log('Dependencies not installed.');
@@ -230,9 +232,8 @@ program
 
         console.log('Finished!');
         console.timeEnd('time used');
-
     })
-    .catch(err => {
+    .catch((err) => {
         console.error(err);
         process.exit(1);
     });
